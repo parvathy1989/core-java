3d02c5982c1962c8b524eaad19336aca4830926f
parvathy1989 <63458473+parvathy1989@users.noreply.github.com>
2020-04-14 16:46:49 +0530

    uploading my project to repository

---
+++ java_training/src/main/java/java_training/Sample.java
@@ -0,0 +1,18 @@
+package java_training;
+
+public class Sample {
+	
+	
+		public static void main(String args[])
+		{
+		
+		int a=4;
+		int b=5;
+		int sum=a+b;
+		System.out.println("The sum is  "+sum);
+
+	}
+	}
+
+
+
